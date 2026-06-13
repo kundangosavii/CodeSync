ef5c9be878e2dddb47ee62a166436f18df5aec6d
Kundan Gosavi <gosavikundan4@gmail.com>
2026-06-13 15:47:02 +0530

    Added fileLoader and fileParser

---
+++ src/core/fileLoader.js
@@ -0,0 +1,23 @@
+import fs from 'fs';
+import path from 'path';
+
+const SUPPORTED_EXTENSIONS = ['.js', '.ts', '.jsx', '.tsx', '.json'];
+
+export function getAllFiles(dirPath, arrayOfFiles = []) {
+    const files = fs.readdirSync(dirPath);
+
+    files.forEach((file) => {
+        const filePath = path.join(dirPath, file);
+
+        if (fs.statSync(filePath).isDirectory()) {
+            getAllFiles(filePath, arrayOfFiles);
+        } else {
+            const ext = path.extname(file);
+            if(SUPPORTED_EXTENSIONS.includes(ext)) {
+                arrayOfFiles.push(filePath);
+            }
+        }   
+    });
+
+    return arrayOfFiles;
+}
