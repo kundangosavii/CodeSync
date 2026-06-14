89905b72ba3cc4385976ea87f3489e0a25bc3388
Kundan Gosavi <gosavikundan4@gmail.com>
2026-06-14 23:47:45 +0530

    Implement getInsightController for getting insights for repo

--- src/api/controller.js
+++ src/api/controller.js
@@ -1,4 +1,5 @@
-import {run} from '../../index.js'
+import {run} from '../../index.js';
+import fs from 'node:fs';
 
 const analyzeController = (req, res) => {
     try {
@@ -11,7 +12,21 @@ const analyzeController = (req, res) => {
 }
 
 const getInsightsController = (req, res) => {
-    cosole.log("Getting insights...")
+    try {
+        const repoId = req.repoId; 
+
+        fs.readFile(`C:/code-analyser/repos/test-project/insights.json`, 'utf-8', (err, data) => {
+            if (err) {
+                console.error('Error reading insights file:', err);
+                return res.status(500).json({ message: 'An error occurred while fetching insights.' }); 
+            }
+            const insights = JSON.parse(data);
+            res.status(200).json(insights);
+        });
+    } catch (error) {
+        console.error('Error fetching insights:', error);
+        res.status(500).json({ message: 'An error occurred while fetching insights.' });
+    }
 }
 
 export { analyzeController, getInsightsController }
