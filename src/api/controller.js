b16baea364307c9a2f4e48a000367a0edb56c314
Kundan Gosavi <gosavikundan4@gmail.com>
2026-06-16 15:23:48 +0530

    Implement UniqueId and register.json logic in controller

--- src/api/controller.js
+++ src/api/controller.js
@@ -1,8 +1,13 @@
 import { run } from '../../index.js';
 import fs from 'node:fs';
+import { __dirname, __filename } from '../../config.js';
+import path from 'path';
 
 const analyzeController = (req, res) => {
     try {
+        const TARGET_DIR = path.join(__dirname, "test-project");
+        const registerPath = path.join(__dirname, "repos", "register.json");
+
         const date = new Date();
         const dateString = date.toLocaleString();
 
@@ -15,15 +20,33 @@ const analyzeController = (req, res) => {
 
         const UniqueId = generateUniqeId();
 
-        run();
+        if (fs.existsSync(`C:/code-analyser/repos/test-project`)) {
+            console.log("repo already analyzed")
+        }
+        else {
+            run()
+            const data = {
+                repoName: path.basename(TARGET_DIR),
+                UniqueId,
+                date: formattedDate
+            }
+            fs.writeFile(registerPath, JSON.stringify(data, null, 2), (err) => {
+                if (err) {
+                    console.error("Error writing to register file:", err);
+                } else {
+                    console.log("Successfully wrote to register file.");
+                }
+            });
+        }
+
         res.status(200).json({
             message: 'Code analysis completed successfully.',
             UniqueId
         });
-    } catch (error) {
-        console.error('Error during code analysis:', error);
-        res.status(500).json({ message: 'An error occurred during code analysis.' });
-    }
+        } catch (error) {
+            console.error('Error during code analysis:', error);
+            res.status(500).json({ message: 'An error occurred during code analysis.' });
+        }
 }
 
 const getInsightsController = (req, res) => {
