4c943039f7597d651dca8198e08647587a6b9023
Kundan Gosavi <gosavikundan4@gmail.com>
2026-06-17 17:41:34 +0530

    Implemented graph transformation function for converting graph into node and edges structure

---
+++ src/core/graphTransformation.js
@@ -0,0 +1,37 @@
+function transformGraph(graphJson){
+    const nodes = []
+    const edges = []
+    const edgeSet = new Set()
+
+    for(const file in graphJson){
+        node.push(
+            {
+                data: {
+                    id: file,
+                    label : file.split("\\").pop()
+                }
+            }
+        )
+
+        const imports = graphJson[file].imports
+
+        imports.forEach((imp) => {
+            const edgeKey = `${file}->${imp}`
+
+            if(!edgeSet.has(edgeKey)){
+            edges.push({
+                data:{
+                    source: file,
+                    target: imp
+                }
+            })
+
+            edgeSet.add(edgeKey)
+        }
+        })
+    }
+
+    return { nodes, edges }
+}
+
+export {transformGraph}
