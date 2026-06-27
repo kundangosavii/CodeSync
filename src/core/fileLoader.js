183f70b25e44d0b010c0d3268248f30e338a34ed
Kundan Gosavi <gosavikundan4@gmail.com>
2026-06-27 15:41:31 +0530

    Added files that not to analysis

--- src/core/fileLoader.js
+++ src/core/fileLoader.js
@@ -3,13 +3,17 @@ import path from 'path';
 
 const SUPPORTED_EXTENSIONS = ['.js', '.ts', '.jsx', '.tsx', '.json'];
 
-const IGNORED_DIRS = ["node_modules", ".git", "dist", "build", ".next"];
+const IGNORED_DIRS = ["node_modules", ".git", "dist", "build", ".next",".gitignore", "package-lock.json", "yarn.lock", "pnpm-lock.yaml", "package.json", "tsconfig.json", ".eslintrc.js", ".prettierrc", ".babelrc", ".env", ".env.local", ".env.development", ".env.test", ".env.production"];
 
 export function getAllFiles(dirPath, arrayOfFiles = []) {
     const files = fs.readdirSync(dirPath);
 
     files.forEach((file) => {
         const filePath = path.join(dirPath, file);
+
+        if (IGNORED_DIRS.includes(file)) {
+            return;
+        }
 
         if (fs.statSync(filePath).isDirectory()) {
             getAllFiles(filePath, arrayOfFiles);
