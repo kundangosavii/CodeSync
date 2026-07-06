718028e0a032194b47d84d4bb9e06570b1364c48
Kundan Gosavi <gosavikundan4@gmail.com>
2026-07-06 22:38:44 +0530

    Enhance analyze command output with detailed file analysis summary

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,8 @@
 import { Command } from 'commander';
 import chalk from 'chalk';
+import path from 'path';
 import { run } from '../index.js';
+import { section } from './helper.js';
 
 
 const program = new Command();
@@ -12,12 +14,20 @@ program
         const result = await run(repoPath);
 
         console.log(chalk.green("✔ Analysis Complete\n"));
+        console.log(chalk.bold.blue(`Found ${result.files.length} files for analysis.\n`));
+
+
+        section("Codebase Summary");
+
+        console.log(`Total Files: ${result.graphNodesEdges.nodes.length}`);
+        console.log(`Entry Points: ${result.insights.EntryPoints.map(ep => path.basename(ep.file)).join(', ')}`);
+        console.log(`Cycles Found: ${result.cycle.length}`);
 
         result.ReadableInsights.forEach((msg, i) => {
             console.log(`${i + 1}. ${msg}`);
         });
 
-        
+
 
     });
 
